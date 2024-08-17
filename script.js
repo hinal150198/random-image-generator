be6670398a4af73cac357aea80887e31f7218765
hinal150198 <36818885+hinal150198@users.noreply.github.com>
2024-08-17 16:52:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-// Assuming your images are named image1.jpg, image2.jpg, ..., image365.jpg
 const totalImages = 110; // Total number of images
 
 function getRandomImage() {
     const randomIndex = Math.floor(Math.random() * totalImages) + 1;
-    const imagePath = `https://hinal150198.github.io/random-image-generator/image${randomIndex}.jpg`; // Update this line
+    const imagePath = `https://hinal150198.github.io/random-image-generator/${randomIndex}.jpg`; // Update this line
     document.getElementById('randomImage').src = imagePath;
 }
 
