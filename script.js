68952d1113849dd7f45b3098c0f5aab455d4d187
hinal150198 <36818885+hinal150198@users.noreply.github.com>
2024-08-17 12:06:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const totalImages = 110;
 // Step 2: Function to generate a random image
 function showRandomImage() {
     const randomIndex = Math.floor(Math.random() * totalImages) + 1; // Generate a random number between 1 and 365
-    const imagePath = `images/${randomIndex}.jpg`; // Construct the path to the image
+    const imagePath = `${randomIndex}.jpg`; // Construct the path to the image
     document.getElementById('randomImage').src = imagePath; // Set the image source
 }
 
