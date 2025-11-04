ad16b69a43c111e616a14f78ab7ff105cfb06da0
hinal150198 <36818885+hinal150198@users.noreply.github.com>
2025-11-03 16:10:50 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const totalImages = 31; // Update with the correct number of images
+const totalImages = 39; // Update with the correct number of images
 
 function getRandomImage() {
     const randomIndex = Math.floor(Math.random() * totalImages) + 1;
